545090f54604b4d7d1a7f883b2717a025a01ded3
Ddev-sourse <devr38954@gmail.com>
2025-01-20 14:00:18 +0530

    Create class12.js

---
+++ class12.js
@@ -0,0 +1,38 @@
+
+window.onload=function()
+        {
+            var a = sessionStorage.getItem("user");
+            if(a==null)
+        {
+            window.location.replace("index.html");
+        
+        }
+        else{
+            
+$(document).ready(function(){
+    $("#box h1").click(function(){
+        $("#box div").slideToggle(1000);
+    });
+
+    $("#box1 h1").click(function(){
+        $("#box1 div").slideToggle(1000);
+    });
+
+    
+    $("#box3 h1").click(function(){
+        $("#box3 div").slideToggle(1000);
+    });
+
+    
+    $("#box4 h1").click(function(){
+        $("#box4 div").slideToggle(1000);
+    });
+
+    $("#arrow").click(function(){
+        window.location="msg.html";
+    });
+});
+        }
+
+    }
+
